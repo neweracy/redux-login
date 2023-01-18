e86bb722bb6d1d3399949142a0e4314f87c3dd54
neweracy <kwakansah2@gmail.com>
2023-01-18 23:52:14 +0000

    first part

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import * as react from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/counter/counterSlice";
 
 const Login = () => {
+  const [name, setName] = react.useState("");
+  const [password, setPassword] = react.useState("");
+  const [email, setEmail] = react.useState("");
+
+  const dispatch = useDispatch();
+  const handleSubmit = (ee: react.FormEvent<HTMLFormElement>) => {
+    ee.preventDefault();
+
+    dispatch(
+      login({
+        name: name,
+        email: email,
+        password: password,
+        loggedIn: true,
+      })
+    );
+  };
+
   return (
     <div className="">
       <div id="Nav" className=""></div>
@@ -16,7 +36,7 @@ const Login = () => {
             Account settings
           </h2>
 
-          <form action="">
+          <form onSubmit={(ee) => handleSubmit(ee)} action="">
             <div className=" grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label
@@ -28,6 +48,8 @@ const Login = () => {
                 <input
                   type="text"
                   placeholder="Username.."
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -41,6 +63,8 @@ const Login = () => {
                 <input
                   type="email"
                   placeholder=" Email Address.. "
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className=" block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -55,11 +79,16 @@ const Login = () => {
               <input
                 type="password"
                 placeholder=" Password.."
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className=" block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
             <div className="flex justify-end mt-6">
-              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              <button
+                type="submit"
+                className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+              >
                 {" "}
                 Submit
                 {""}
