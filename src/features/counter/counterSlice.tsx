6a80b22f60ee976da33137d7d396eeec7cd6ba67
neweracy <kwakansah2@gmail.com>
2023-01-19 18:06:21 +0000

    done

--- src/features/counter/counterSlice.tsx
+++ src/features/counter/counterSlice.tsx
@@ -1,4 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { write } from "fs";
+import { Root } from "react-dom/client";
 import { RootState, AppThunk } from "../../app/store";
 //import { fetchCount } from "./counterAPI";
 
@@ -23,7 +25,7 @@ export const { login, logout } = counterSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectUser = (state: RootState) => state.counter.user;
+export const selectUser = (state: { counter: { user: any; }; }) => state.counter.user;
 console.log(selectUser);
 
 // We can also write thunks by hand, which may contain both sync and async logic.
